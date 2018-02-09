6f6f4b453b3a9b01a0a5b3e5e6aceeaa420f970e
bayweiheng <bayweiheng@gmail.com>
2018-02-09 20:23:42 +0800

    Add dummy SortCommand that is accepted by program but does nothing, updated help message and parser/diff tests

---
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -0,0 +1,22 @@
+package seedu.addressbook.commands;
+
+/**
+ * Clears the address book.
+ */
+public class SortCommand extends Command {
+
+    public static final String COMMAND_WORD = "sort";
+    public static final String MESSAGE_USAGE = "Sorts address book by name. Persons with the same name" +
+            "will have their relative order preserved. \n"
+            + "Example: " + COMMAND_WORD;
+
+    public static final String MESSAGE_SUCCESS = "All persons in address book sorted!";
+
+    public SortCommand() {}
+
+
+    @Override
+    public CommandResult execute() {
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
